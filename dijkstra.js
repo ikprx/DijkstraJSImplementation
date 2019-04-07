72754a9ffc11eed32df2703bfe8c25da24b8966a
ikprx <ikprx@github.com>
2019-04-07 12:30:40 +0200

    graph properly implemented

---
+++ dijkstra.js
@@ -0,0 +1,207 @@
+// TUTAJ IMPLEMENTUJE MIN-HEAP
+// NIE JEST TO KONIECZNE DO ZROZUMIENIA DIJKSTRA!
+function MinHeap(){
+    // elementy naszego heapa
+    this.items = new Array();
+}
+//lewa galaz od rodzica
+MinHeap.prototype.getLeftChildIndex = function(parentIndex)
+{
+        return (2*parentIndex) + 1;
+};
+//prawa galaz od rodzica
+MinHeap.prototype.getRightChildIndex = function(parentIndex)
+{
+    return (2*parentIndex) + 2;
+};
+// zwroc rodzica
+MinHeap.prototype.getParentIndex = function(childIndex)
+{
+    return Math.ceil((childIndex-1) / 2);
+};
+// ma lewa galaz ?
+MinHeap.prototype.hasLeftChild = function(index)
+{
+    return getLeftChildIndex(index) < this.items.length;
+};
+// ma prawa galaz ?
+MinHeap.prototype.hasLeftChild = function(index)
+{
+    return getRightChildIndex(index) < this.items.length;
+};
+//ma rodzica ? 
+MinHeap.prototype.hasParent = function(index)
+{
+    return this.getParentIndex(index) >= 0;
+};
+//zwroc lewa galaz - wartosc nie indeksach!
+MinHeap.prototype.leftChild = function(index)
+{
+    return this.items[this.getLeftChildIndex(index)];
+};
+//zwroc prawa galaz - wartosc nie indeksach!
+MinHeap.prototype.rightChild = function(index)
+{
+    return this.items[this.getRightChildIndex(index)];
+};
+//zwroc rodzica - wartosc nie indeksach!
+MinHeap.prototype.parent = function(index)
+{
+    return this.items[this.getParentIndex(index)];
+};
+// zamien dwa elementy w poszczegolnych indeksach
+MinHeap.prototype.swap = function(indexOne, indexTwo)
+{
+    var tmp = this.items[indexOne];
+    this.items[indexOne] = this.items[indexTwo];
+    this.items[indexTwo] = tmp;
+};
+//zwroc wartosc pierwszego elementu
+MinHeap.prototype.peek = function()
+{
+    if(this.items.length != 0)
+    {
+        return this.items[0];
+    }
+    else
+    {
+        console.log("MinHeap:items: JEST PUSTY!")
+    }
+};
+//popraw kolejnosc w heapie
+MinHeap.prototype.heapifyUp = function()
+{
+    var index = this.items.length -1;
+    while(this.hasParent(index) && this.parent(index) > this.items[index])
+    {
+        this.swap(this.getParentIndex(index),index);
+        index = this.getParentIndex(index);
+    }
+};
+//popraw kolejnosc w heapie
+MinHeap.prototype.heapifyDown = function()
+{
+    var index = 0;
+    while(this.hasLeftChild(index))
+    {
+        var smallerChildIndex = this.getLeftChildIndex(index);
+        if(this.hasRightChild(index) && this.rightChild(index) > this.leftChild(index))
+        {
+            smallerChildIndex = this.getRightChildIndex(index);
+        }
+        if(this.items[index] < items[smallerChildIndex])
+        {
+            break;   
+        }
+        else{
+            swap(index,smallerChildIndex);
+        }
+        index = smallerChildIndex;
+    }
+};
+// zwroc element, nie tylko wartosc 
+MinHeap.prototype.poll = function()
+{
+
+    if(this.items.length != 0)
+    {
+        var tmp = this.items[0];
+        this.items[0] = this.items[this.items.length-1]; 
+        this.items.pop();
+        heapifyDown();
+        return tmp;
+    }
+    else
+    {
+        console.log("MinHeap:items: JEST PUSTY!")
+    }
+};
+// dodaj element
+MinHeap.prototype.add = function(item)
+{
+    this.items.push(item);
+    this.heapifyUp();
+};
+//Obiekt krawedz
+// u - poczatek
+// v- koniec
+// w - koszt
+function Edge(u,v,w)
+{
+    this.u = u;
+    this.v = v;
+    this.w = w;
+}
+
+function Graph()
+{
+    //uzywamy slownikow to reprezentacji grafu
+    //wierzcholki
+    // wszystkie wierzcholki
+    // kazdy wierzchoek opsiany przez strukture
+    // d - koszt od s(source)
+    // pred - poprzednik w kierunku punktu s
+    this.vertices = new Map();
+    //tablica sasiedztwa
+    // adj[u] = sasiedzi krawedzi u
+    this.adj = new Map(); 
+    //kosztu poszczegolnych krawedzi
+    // wages[u][v] = koszt
+    this.wages = new Map();
+}
+
+Graph.prototype.processEdge = function(e)
+{
+    if(!this.adj.has(e.u))
+    {
+        this.adj.set(e.u,[]);
+    }
+    if(!this.adj.has(e.v))
+    {
+        this.adj.set(e.v,[]);
+    }
+
+    this.adj.get(e.u).push(e.v);
+    this.adj.get(e.v).push(e.u);
+
+    if(!this.wages.has(e.u))
+    {
+        this.wages.set(e.u,new Map());
+    }
+    this.wages.get(e.u).set(e.v, e.w);
+
+    if(!this.vertices.has(e.u)){
+        this.vertices.set(e.u, new Map());
+        this.vertices.get(e.u).set("d",Infinity);
+        this.vertices.get(e.u).set("pred",null);
+    }
+    if(!this.vertices.has(e.v)){
+        this.vertices.set(e.v, new Map());
+        this.vertices.get(e.v).set("d",Infinity);
+        this.vertices.get(e.v).set("pred",null);
+    }
+}
+
+function dijkstra(graph, source)
+{
+
+}
+
+window.onload = function()
+{
+    var h = new MinHeap();
+    h.add(45);
+    h.add(50);
+    h.add(40);
+    h.add(10);
+    h.add(2313210);
+
+
+    var graph = new Graph();
+    graph.processEdge(new Edge("x","t",10));
+    graph.processEdge(new Edge("x","y",5));
+    console.log(graph.adj);
+    console.log(graph.wages);
+    console.log(graph.vertices);
+    console.log(h.peek());
+};
